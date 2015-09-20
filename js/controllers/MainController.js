6b94a49fb9f256fa252db208a0978d83939d114a
chinlam91 <chinlam91@hotmail.com>
2015-09-20 17:29:37 +0800

    vms page register privacy

---
+++ js/controllers/MainController.js
@@ -0,0 +1,7 @@
+app.controller('MainController', ['$scope', function($scope) {
+    $scope.vms = 'VMS';
+    $scope.login = 'Login';
+    $scope.register = 'Register';
+    $scope.privacy = 'Privacy (/prɪvəsi/ or /praɪvəsi/; from Latin: privatus) is the ability of an individual or group to seclude themselves, or information about themselves, and thereby express themselves selectively. The boundaries and content of what is considered private differ among cultures and individuals, but share common themes. When something is private to a person, it usually means that something is inherently special or sensitive to them. The domain of privacy partially overlaps security (confidentiality), which can include the concepts of appropriate use, as well as protection of information. Privacy may also take the form of bodily integrity.';
+
+}]);
