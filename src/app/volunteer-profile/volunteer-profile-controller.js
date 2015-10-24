acbc610ed024901e1debb93fc005d8a913e03aed
chinlam91 <chinlam91@hotmail.com>
2015-10-25 05:03:57 +0800

    Delete account dialog done

--- src/app/volunteer-profile/volunteer-profile-controller.js
+++ src/app/volunteer-profile/volunteer-profile-controller.js
@@ -6,16 +6,40 @@
         .controller('ProfileController', ProfileController);
 
     /** @ngInject */
-    function ProfileController($uibModal) {
-        var vm = this;
 
+    function ProfileController($uibModal) {
+
+        var vm = this;
         vm.volunteer = {
             img: "profile.png",
             name: "Jim Lim",
             introduction: "I'm a genius. I work on Data science/analytics and have excellent skills with Matlab and Ruby programming. My hobbies is sporting.",
             participating: "3",
             participated: "8"
+        };
+
+        vm.open = function() {
+            var modal = $uibModal.open({
+                templateUrl: 'myModalContent.html',
+                controller: 'ModalInstanceCtrl',
+                controllerAs: 'vm'
+            });
+        }
     }
 
-
 })();
+
+angular.module('vmsFrontend').controller('ModalInstanceCtrl', function($uibModalInstance) {
+
+    var vm = this;
+    vm.remind = "Warning: Account deletion cannot be undone. If you're absolutely sure you want to delete your account, enter your password to continue:";
+
+    vm.ok = function() {
+       // alert(vm.password); //testing ng-model password
+        $uibModalInstance.close();
+    };
+
+    vm.cancel = function() {
+        $uibModalInstance.dismiss('cancel');
+    };
+});
