3ead6708049233880246e4d68de82d34e2029e68
chinlam91 <chinlam91@hotmail.com>
2015-10-24 22:16:34 +0800

    Done Volunteer Profile left bar with delete account layout

--- src/app/volunteer-profile/volunteer-profile-controller.js
+++ src/app/volunteer-profile/volunteer-profile-controller.js
@@ -9,5 +9,14 @@
     function ProfileController() {
         var vm = this;
         vm.img = "profile.png";
+
+
+        vm.volunteer = {
+            image: "../../../assets/img/profile.png",
+            name: "Jim Lim",
+            introduction: "I'm a genius. I work on Data science/analytics and have excellent skills with Matlab and Ruby programming. My hobbies is sporting.",
+            participating: "3",
+            participated: "8",
+        };
     }
 })();
