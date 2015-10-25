fa1ac93acf35e41b0957f5e234dbd237dd098e91
chinlam91 <chinlam91@hotmail.com>
2015-10-25 18:33:52 +0800

    Added volunteer profile tab 1 content and wrote all the style to scss file

--- src/app/volunteer-profile/volunteer-profile-controller.js
+++ src/app/volunteer-profile/volunteer-profile-controller.js
@@ -11,11 +11,42 @@
 
         var vm = this;
         vm.volunteer = {
-            img: "profile.png",
+            img: "/assets/images/profile-default-avatar.png",
             name: "Jim Lim",
             introduction: "I'm a genius. I work on Data science/analytics and have excellent skills with Matlab and Ruby programming. My hobbies is sporting.",
             participating: "3",
             participated: "8"
+        };
+
+        vm.profile = {
+            about: {
+                firstName: "Lin",
+                lastName: "Jim",
+                userName: "jimlin",
+                gender: "Male",
+                address: "128 Academia Road, Section2, Nankang Dist.",
+                birthDate: "1990/05/05",
+                city: "Taipei City"
+            },
+            contact: {
+                phoneNumber: "0912345678",
+                emailAddress: "jimlin@citi.sinica.edu.tw",
+                emergencyContact: "Jeremy Lin",
+                emergencyPhone: "0919119119"
+            },
+            experience: {
+                researchAssistant: "Research assistant",
+                position: "Academia Sinica",
+                date: "2014-now"
+            },
+            education: {
+                background: "Master's degree, Computer science",
+                school: "NCKU",
+                schoolDate: "2012-2014"
+            },
+            skills: ["Swimming", "Programming"],
+            equipments: ["Car", "Scooter", "Camera"]
+
         };
 
         vm.open = function() {
@@ -25,6 +56,8 @@
                 controllerAs: 'vm'
             });
         }
+
+
     }
 
 })();
@@ -35,7 +68,7 @@ angular.module('vmsFrontend').controller('ModalInstanceCtrl', function($uibModal
     vm.remind = "Warning: Account deletion cannot be undone. If you're absolutely sure you want to delete your account, enter your password to continue:";
 
     vm.ok = function() {
-       // alert(vm.password); //testing ng-model password
+        // alert(vm.password); //testing ng-model password
         $uibModalInstance.close();
     };
 
