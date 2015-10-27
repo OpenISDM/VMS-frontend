654fc985a00c2147f0e07539cb49353db65076b5
YMHuang <ym.huang0808@gmail.com>
2015-10-27 14:40:01 +0800

    change Restangular base url into constant

--- src/app/index.config.js
+++ src/app/index.config.js
@@ -6,9 +6,9 @@
     .config(config);
 
   /** @ngInject */
-  function config($logProvider, toastrConfig, RestangularProvider, localStorageServiceProvider) {
+  function config($logProvider, toastrConfig, RestangularProvider, localStorageServiceProvider, apiBaseUrl) {
     localStorageServiceProvider.setPrefix('vms');
-    RestangularProvider.setBaseUrl('https://vms-dev.herokuapp.com/api');
+    RestangularProvider.setBaseUrl(apiBaseUrl);
     //RestangularProvider.setDefaultHeaders( {'X-VMS-API-Key': '581dba93a4dbafa42a682d36b015d8484622f8e3543623bec5a291f67f5ddff1'} );
 
     // Enable log
