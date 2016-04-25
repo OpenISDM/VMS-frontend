5c956a1413d3ae962982d8478a69d4e505091ffb
YMHuang <ym.huang0808@gmail.com>
2016-04-25 17:16:45 +0800

    Add formatDateTime filter

---
+++ src/app/components/formatDateTime/formatDateTime.directive.js
@@ -0,0 +1,22 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('vmsFrontend')
+    .filter('formatDateTime', formatDateTime);
+
+  /** @ngInject */
+  function formatDateTime($filter, $log) {
+    var filter = function(dateString) {
+      var dateValue = new Date(dateString);
+      var transformedInput = $filter('date')(dateValue, 'yyyy/MM/dd hh:mm');
+
+      $log.debug('=== formatDateTime directive ===');
+      $log.debug(transformedInput);
+
+      return transformedInput;
+    };
+
+    return filter;
+  }
+})();
